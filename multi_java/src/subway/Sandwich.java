bbeb27161287b835d4dd09bd49694a6561ed2325
SangHwan <tkdghks4564@naver.com>
2022-07-27 16:29:11 +0900

    2022-07-27 수정

--- multi_java/src/subway/Sandwich.java
+++ multi_java/src/subway/Sandwich.java
@@ -1,15 +1,6 @@
 package subway;
 
-<<<<<<< HEAD
-//<<<<<<< HEAD
-//=======
  
 abstract public class Sandwich {
 	abstract public String choice(String menu);
-// >>>>>>> branch 'Sanghwan' of https://github.com/multi-java/java.git
-=======
-public class Sandwich {
-	//확인 Bomi --
-
->>>>>>> refs/remotes/origin/Bomi
 }
